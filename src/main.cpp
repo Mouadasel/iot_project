485afdd542ba0dc16954229c1ecdf9758df69981
Admin <admin@Admins-MacBook-Pro.local>
2022-07-25 16:01:53 +0100

    main programme

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+int red=14;
+int green=26;
+int yellow=13;
+
 
 void setup() {
   // put your setup code here, to run once:
+pinMode(red,OUTPUT);
+pinMode(green,OUTPUT);
+pinMode(yellow,OUTPUT);
+Serial.begin(9600);
+Serial.print("setup\n");
 }
 
 void loop() {
+  Serial.print("loop\n");
+
   // put your main code here, to run repeatedly:
+digitalWrite(red,HIGH);
+digitalWrite(yellow,LOW);
+delay(3000);
+digitalWrite(green,HIGH);
+digitalWrite(red,LOW);
+delay(3000);
+digitalWrite(yellow,HIGH);
+digitalWrite(green,LOW);
+delay(1000);
 }
